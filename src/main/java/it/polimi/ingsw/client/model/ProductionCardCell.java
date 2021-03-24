5dd7ce5a5c7003b6f70deca3377537ea9cd7dee2
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-03-24 21:37:59 +0100

    Added ProductionCardCell class (NO TEST)

---
+++ src/main/java/it/polimi/ingsw/client/model/ProductionCardCell.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.client.model;
+
+/**
+ * Container class for Development Cards, similiar to a stack
+ * @author Gianmarco Lodari
+ */
+
+public class ProductionCardCell {
+    private DevelopmentCard[] cards;
+    int stackHeight;
+
+    /**
+     * Returns the card on top of the stack
+     */
+    public DevelopmentCard getFrontCard()
+    {
+        return cards[stackHeight];
+    }
+
+    /**
+     * Checks if the stack is full
+     */
+    public boolean isSpotAvailable()
+    {
+        return stackHeight < 3;
+    }
+
+    /**
+     * Adds one to the stack
+     * @param card!=NULL
+     */
+    public void addToTop(DevelopmentCard card)
+    {
+        cards[stackHeight]=card;
+        stackHeight++;
+    }
+}
