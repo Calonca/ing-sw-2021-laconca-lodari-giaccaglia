c8af9db95e475a85750431861eff30306d2d20ca
pablonvibe <pablo.giaccaglia@mail.polimi.it>
2021-03-24 23:04:36 +0100

    added Resource and FaithZone to create model and player subpackages

---
+++ src/main/java/it/polimi/ingsw/client/model/player/FaithZone.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.client.model.player;
+
+public enum FaithZone {
+    NORMAL(-1),
+    ZONE1(0),
+    ZONE2(1),
+    ZONE3(2);
+
+    private final int zoneNumber;
+
+    FaithZone(final int zoneNumber) {
+        this.zoneNumber = zoneNumber;
+    }
+
+    public int getZoneNumber() {
+        return this.zoneNumber;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/client/model/player/board/Resource.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.client.model.player.board;
+
+public enum Resource {
+
+    SERVANT,
+    STONE,
+    SHIELD,
+    GOLD,
+    FAITH_POINT,
+    TOCHOOSE,
+    CHOOSE_SERVANT,
+    CHOOSE_STONE,
+    CHOOSE_SHIELD,
+    CHOOSE_GOLD,
+    WHITE,
+    EMPTY,
+    BADFAITHPOINT
+
+}
