7fa85fcfb32ae6ac09d83502d9692f54163ec22f
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-03-24 22:02:36 +0100

    Added DevelopmentCard class

---
+++ src/main/java/it/polimi/ingsw/client/model/DevelopmentCard.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.client.model;
+
+/**
+ * Class for Development Cards. Since the color is not strictly tied to the input/output we totally decoupled that
+ * @author Gianmarco Lodari
+ */
+public class DevelopmentCard
+{
+    private int level;
+
+    //private Production production;
+    /**
+     * Class to effectively use the card, yet to be added
+     */
+    private DevelopmentCardColor cardType;
+    /**
+     * Enum to indicate card color
+     */
+    // private CostCell costcell;
+
+
+
+}
