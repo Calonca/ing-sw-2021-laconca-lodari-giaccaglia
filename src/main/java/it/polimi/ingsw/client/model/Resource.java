f703a24f8a8becc6809dbe76af030a2be0ee73b3
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-03-24 19:50:51 +0100

    Adding Resource ENUM

---
+++ src/main/java/it/polimi/ingsw/client/model/Resource.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.client.model;
+
+/**
+ * Enum class for resources. Resources order is the same as in Box.
+ * New resources are to be added after STONE.
+ * For each TOCHOOSE the player will perform a choice, selecting a basic resource.
+ * Each BADFAITH point will be consumed and all other players will advance on the FaithTrack.
+ * @author PSP10
+ */
+
+public enum Resource
+{
+    GOLD,
+    SERVANT,
+    SHIELD,
+    STONE,
+    FAITH,
+    BADFAITH,
+    TOCHOOSE,
+    WHITEMARBLE,
+}
