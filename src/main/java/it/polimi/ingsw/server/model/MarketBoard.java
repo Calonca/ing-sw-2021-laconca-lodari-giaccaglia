f1efeefab2d991e36931ed288e8d58aa103e2d32
calonca <alexlaconca@gmail.com>
2021-03-26 12:00:11 +0100

    Randomized marbleMatrix creation and added default initializer

--- src/main/java/it/polimi/ingsw/server/model/MarketBoard.java
+++ src/main/java/it/polimi/ingsw/server/model/MarketBoard.java
@@ -2,13 +2,17 @@ package it.polimi.ingsw.server.model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import javafx.util.Pair;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MarketBoard {
 
@@ -20,39 +24,49 @@ public class MarketBoard {
     public static MarketBoard initializeMarketBoard(String configFilePath) {
         Gson gson = new Gson();
         MarketBoard test = null;
-
         try {
+
         String MarketBoardClassConfig = Files.readString(Path.of(configFilePath), StandardCharsets.US_ASCII);
         test = gson.fromJson(MarketBoardClassConfig, MarketBoard.class);
+        List<Marble> marbles  = Arrays
+                .stream(test.marbleMatrix)
+                .flatMap(Arrays::stream).collect(Collectors.toList());
 
+        Collections.shuffle(marbles);
+        final int rows = test.rows;
+            test.marbleMatrix = IntStream.range(0,test.columns* test.rows)
+                .mapToObj((pos)->new Pair<>(pos,marbles.get(pos)))
+                .collect(Collectors.groupingBy((e)->e.getKey()% rows))
+                .values()
+                .stream()
+                .map(MarketBoard::pairToValue)
+                    .toArray(Marble[][]::new);
+        
+        
         } catch (IOException e) {
             System.out.println("Error while class initialization with json config file at path: " + configFilePath);
             e.printStackTrace();
         }
+
+
         return test;
     }
 
-
-    public static void main(String[] args) throws IOException {
-
-        MarketBoard resourcesMarket = MarketBoard.initializeMarketBoard("MarketBoardConfigPATH");
-
-        // List<Marble[]> testList = Arrays.asList(resourcesMarket.marbleMatrix);
-
-        Marble[] test  = (Arrays
-                .stream(resourcesMarket.marbleMatrix)
-                .flatMap(Arrays::stream)
-                .toArray(Marble[]::new));
-
-        Util.shuffleArray(test);
+    private static Marble[] pairToValue(List<Pair<Integer, Marble>> pos_marArray){
+        return pos_marArray.stream().map(Pair::getValue).toArray(Marble[]::new);
+    }
 
 
-        Gson gson = new GsonBuilder().create();
-        Writer writer = new FileWriter("MareblesArray.json");
-        gson.toJson(test, writer);
-        System.out.println(5);
-        writer.flush(); //flush data to file   <---
-        writer.close(); //close write
+    public static MarketBoard initializeMarketBoard(){
+        File file = new File("src/main/resources/config/MarketBoardConfig.json");
+        return initializeMarketBoard(file.getAbsolutePath());
+    }
+
+
+    public static void main(String[] args) {
+
+        MarketBoard resourcesMarket = initializeMarketBoard();
+        System.out.println(Arrays.deepToString(resourcesMarket.marbleMatrix));
     }
 }
 
