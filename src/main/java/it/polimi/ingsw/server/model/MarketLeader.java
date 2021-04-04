8e94fd0f3b944b77f2e0bd111b029bdf41f38774
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-04-04 11:20:51 +0200

    Fixed a typo in JavaDoc

---
+++ src/main/java/it/polimi/ingsw/server/model/MarketLeader.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.server.model;
+
+import it.polimi.ingsw.server.model.player.LeaderState;
+
+/**
+ * This Leader class doesn't need extra methods
+ */
+
+public class MarketLeader extends Leader
+{
+    private LeaderState state;
+    private Resource bonus;
+}
