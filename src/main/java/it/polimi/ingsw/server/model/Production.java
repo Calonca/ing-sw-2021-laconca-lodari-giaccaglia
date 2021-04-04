a6e57efa947da2fafbaa374e44c128ccc94affd7
calonca <alexlaconca@gmail.com>
2021-04-04 22:44:38 +0200

    Make javadoc clearer

--- src/main/java/it/polimi/ingsw/server/model/Production.java
+++ src/main/java/it/polimi/ingsw/server/model/Production.java
@@ -95,7 +95,7 @@ public class Production {
     /**
      * Return an array with the outputs of the production
      * @return an array of int with the outputs of the production.
-     * In position i of the array there is the number of resources in position i of the resource ordering
+     * In position i of the array there is the number of resources of type i in the enum ordering
      */
     public int[] getOutputs(){
         return output;
