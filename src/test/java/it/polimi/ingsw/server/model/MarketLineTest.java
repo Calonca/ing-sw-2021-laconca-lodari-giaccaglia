384d3c55e4b069b448c787ee79b5fccc499f996e
pablonvibe <pablo.giaccaglia@mail.polimi.it>
2021-04-05 18:45:56 +0200

    Added MarketLineTest

---
+++ src/test/java/it/polimi/ingsw/server/model/MarketLineTest.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.server.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MarketLineTest {
+
+    @Test
+    public void getLineNumber() {
+        MarketLine [] lines = MarketLine.values();
+
+        for(MarketLine line : lines){
+            assertEquals(line.getLineNumber(), line.ordinal());
+        }
+    }
+
+    @Test
+    public void valueOf() {
+        assertNotNull(MarketLine.valueOf("FIRST_ROW"));
+        assertNotNull(MarketLine.valueOf("SECOND_ROW"));
+        assertNotNull(MarketLine.valueOf("THIRD_ROW"));
+        assertNotNull(MarketLine.valueOf("FIRST_COLUMN"));
+        assertNotNull(MarketLine.valueOf("SECOND_COLUMN"));
+        assertNotNull(MarketLine.valueOf("THIRD_COLUMN"));
+    }
+}
