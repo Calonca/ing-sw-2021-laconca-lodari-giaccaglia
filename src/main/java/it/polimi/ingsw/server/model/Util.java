bac154faf6634496479b27ab93a1c048f5d48364
pablonvibe <pablo.giaccaglia@mail.polimi.it>
2021-04-09 00:11:50 +0200

    added CardShop json config and related Util's method

--- src/main/java/it/polimi/ingsw/server/model/Util.java
+++ src/main/java/it/polimi/ingsw/server/model/Util.java
@@ -1,12 +1,17 @@
 package it.polimi.ingsw.server.model;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import it.polimi.ingsw.server.model.cards.CardShop;
 import it.polimi.ingsw.server.model.cards.DevelopmentCard;
 import it.polimi.ingsw.server.model.cards.DevelopmentCardColor;
+import it.polimi.ingsw.server.model.cards.DevelopmentCardDeck;
 import it.polimi.ingsw.server.model.market.Marble;
+import javafx.util.Pair;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -47,6 +52,7 @@ public class Util
         return IntStream.range(0,len).map((i)->aa[i]+ bb[i]).toArray();
     }
 
+    //helper method to load a 48 devcards array from json
     private List<DevelopmentCard> cardsFromJsonHandler(){
         Gson gson = new Gson();
         String Cards = null;
@@ -60,17 +66,91 @@ public class Util
 
     }
 
-    public static void main(String[] args) {
+    //helper method to build CardShop json initialization from an array of 48 devcards
+    public static void main(String[] args) throws IOException {
 
         Util util = new Util();
+        int cardsInDeck = 4;
         List<DevelopmentCard> cards = util.cardsFromJsonHandler();
-        List<DevelopmentCard> blue = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.BLUE).collect(Collectors.toList());
-        List<DevelopmentCard> yellow = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.YELLOW).collect(Collectors.toList());
-        List<DevelopmentCard> green = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.GREEN).collect(Collectors.toList());
-        List<DevelopmentCard> purple = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.PURPLE).collect(Collectors.toList());
+        List<DevelopmentCard> blueCards = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.BLUE).collect(Collectors.toList());
+        List<DevelopmentCard> yellowCards = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.YELLOW).collect(Collectors.toList());
+        List<DevelopmentCard> greenCards = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.GREEN).collect(Collectors.toList());
+        List<DevelopmentCard> purpleCards = cards.stream().filter(card -> card.getCardType() == DevelopmentCardColor.PURPLE).collect(Collectors.toList());
 
-      //  List<DevelopmentCard> blue1 = blue.stream().filter(blue -> blue.getLevel() == 1)
-        int color = 5;
+        List<List<DevelopmentCard>> decksByColor = new ArrayList<>();
+        decksByColor.add(blueCards);
+        decksByColor.add(yellowCards);
+        decksByColor.add(greenCards);
+        decksByColor.add(purpleCards);
+
+        List<List<DevelopmentCard>> blueCardsByLevel = IntStream.range(1, cardsInDeck).mapToObj(i ->
+                ((blueCards.stream().filter(blueCard -> blueCard.getLevel()==i))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+        List<List<DevelopmentCard>> yellowCardsByLevel = IntStream.range(1, cardsInDeck).mapToObj(i ->
+                ((yellowCards.stream().filter(yellowCard -> yellowCard.getLevel()==i))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+        List<List<DevelopmentCard>> greenCardsByLevel = IntStream.range(1, cardsInDeck).mapToObj(i ->
+                ((greenCards.stream().filter(greenCard -> greenCard.getLevel()==i))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+        List<List<DevelopmentCard>> purpleCardsByLevel = IntStream.range(1, cardsInDeck).mapToObj(i ->
+                ((purpleCards.stream().filter(purpleCard -> purpleCard.getLevel()==i))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+        Map<DevelopmentCardColor, List<List<DevelopmentCard>>> decksOfCardsByColor = new HashMap<>();
+
+        decksOfCardsByColor.put(DevelopmentCardColor.BLUE, blueCardsByLevel);
+        decksOfCardsByColor.put(DevelopmentCardColor.YELLOW, yellowCardsByLevel);
+        decksOfCardsByColor.put(DevelopmentCardColor.GREEN, greenCardsByLevel);
+        decksOfCardsByColor.put(DevelopmentCardColor.PURPLE, purpleCardsByLevel);
+
+        Map<Integer, List<DevelopmentCard>> blueCardsList = IntStream.range(1, cardsInDeck).boxed()
+                .collect(Collectors.toMap(
+                level -> level, level -> decksOfCardsByColor.get(DevelopmentCardColor.BLUE).get(level-1)));
+
+        Map<Integer, List<DevelopmentCard>> yellowCardsList = IntStream.range(1, cardsInDeck).boxed()
+                .collect(Collectors.toMap(
+                        level -> level, level -> decksOfCardsByColor.get(DevelopmentCardColor.YELLOW).get(level-1)));
+
+        Map<Integer, List<DevelopmentCard>> greenCardsList = IntStream.range(1, cardsInDeck).boxed()
+                .collect(Collectors.toMap(
+                        level -> level, level -> decksOfCardsByColor.get(DevelopmentCardColor.GREEN).get(level-1)));
+
+        Map<Integer, List<DevelopmentCard>> purpleCardsList = IntStream.range(1, cardsInDeck).boxed()
+                .collect(Collectors.toMap(
+                        level -> level, level -> decksOfCardsByColor.get(DevelopmentCardColor.PURPLE).get(level-1)));
+
+        Map<Integer, DevelopmentCardDeck> blueDecks = IntStream.range(1, cardsInDeck).boxed().collect(Collectors.toMap(level -> level, level ->
+                new DevelopmentCardDeck(level, DevelopmentCardColor.BLUE, blueCardsList.get(level), blueCardsList.get(level).size(), blueCardsList.get(level).size())));
+
+        Map<Integer, DevelopmentCardDeck> purpleDecks = IntStream.range(1, cardsInDeck).boxed().collect(Collectors.toMap(level -> level, level ->
+                new DevelopmentCardDeck(level, DevelopmentCardColor.PURPLE, purpleCardsList.get(level), purpleCardsList.get(level).size(), purpleCardsList.get(level).size() )));
+
+        Map<Integer, DevelopmentCardDeck> yellowDecks = IntStream.range(1, cardsInDeck).boxed().collect(Collectors.toMap(level -> level, level ->
+                new DevelopmentCardDeck(level, DevelopmentCardColor.YELLOW, yellowCardsList.get(level), yellowCardsList.get(level).size(), yellowCardsList.get(level).size() )));
+
+        Map<Integer, DevelopmentCardDeck> greenDecks = IntStream.range(1, cardsInDeck).boxed().collect(Collectors.toMap(level -> level, level ->
+                new DevelopmentCardDeck(level, DevelopmentCardColor.GREEN, greenCardsList.get(level), greenCardsList.get(level).size(), greenCardsList.get(level).size() )));
+
+        Map<DevelopmentCardColor, Map<Integer, DevelopmentCardDeck>> listOfDecks = new HashMap<>();
+        listOfDecks.put(DevelopmentCardColor.BLUE, blueDecks);
+        listOfDecks.put(DevelopmentCardColor.PURPLE, purpleDecks);
+        listOfDecks.put(DevelopmentCardColor.YELLOW, yellowDecks);
+        listOfDecks.put(DevelopmentCardColor.GREEN, greenDecks);
+
+        CardShop shop = new CardShop(listOfDecks);
+
+        Writer writer = new FileWriter("src/main/resources/config/CardShopConfig.json");
+        Gson gson = new GsonBuilder().create();
+        gson.toJson(shop, writer);
+        writer.flush(); //flush data to file   <---
+        writer.close(); //close write          <---
     }
 
 
