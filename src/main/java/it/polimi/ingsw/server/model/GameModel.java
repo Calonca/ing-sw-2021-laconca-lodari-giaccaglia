fb3207f6354c7797344299556b5c580b3077e787
calonca <alexlaconca@gmail.com>
2021-04-08 21:40:13 +0200

    Fix bug getting causing online players list size reduction

--- src/main/java/it/polimi/ingsw/server/model/GameModel.java
+++ src/main/java/it/polimi/ingsw/server/model/GameModel.java
@@ -66,7 +66,7 @@ public class GameModel {
     }
 
     private void updateOnlinePlayers() {
-        onlinePlayers = onlinePlayers
+        onlinePlayers = players
                 .stream()
                 .filter(Player::isOnline)
                 .collect(Collectors.toList());
