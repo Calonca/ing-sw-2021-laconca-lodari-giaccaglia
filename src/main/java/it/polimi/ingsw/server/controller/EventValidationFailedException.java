ae6557d5cb6d8089ba267969e281d827148c97f5
calonca <alexlaconca@gmail.com>
2021-04-16 18:47:33 +0200

    Initial commit

---
+++ src/main/java/it/polimi/ingsw/server/controller/EventValidationFailedException.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.server.controller;
+
+/**
+ * It is thrown when a resource is added to a non empty space in the depots
+ */
+
+public class EventValidationFailedException extends Exception{
+    EventValidationFailedException(){super();}
+
+    EventValidationFailedException(String message){
+        super(message);
+    }
+}
