766ef3c82cc805ae04877560b5d0a950b2fe1c84
calonca <alexlaconca@gmail.com>
2021-05-02 13:10:20 +0200

    StatesTransitionTable working

--- src/main/java/it/polimi/ingsw/server/model/StatesTransitionTable.java
+++ src/main/java/it/polimi/ingsw/server/model/StatesTransitionTable.java
@@ -1,51 +1,130 @@
 package it.polimi.ingsw.server.model;
 
 import com.google.gson.Gson;
-import it.polimi.ingsw.server.controller.strategy.GameStrategy;
+import com.google.gson.GsonBuilder;
+import it.polimi.ingsw.RuntimeTypeAdapterFactory;
+import it.polimi.ingsw.server.controller.strategy.*;
+import it.polimi.ingsw.server.controller.strategy.leader.ActivateLeader;
+import it.polimi.ingsw.server.controller.strategy.leader.DiscardLeader;
+import it.polimi.ingsw.server.controller.strategy.leader.EndLeaders;
 import it.polimi.ingsw.server.messages.clienttoserver.events.Validable;
+import it.polimi.ingsw.server.messages.clienttoserver.events.leaderphaseevent.DiscardLeaderEvent;
+import it.polimi.ingsw.server.messages.clienttoserver.events.leaderphaseevent.PlayLeaderEvent;
+import it.polimi.ingsw.server.messages.clienttoserver.events.leaderphaseevent.SkipLeaderEvent;
+import it.polimi.ingsw.server.messages.clienttoserver.events.setupphaseevent.SetupPhaseEvent;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
+
 public class StatesTransitionTable {
-    private Map<State, Map<Validable, GameStrategy>> table;
+    /**
+     * The String is the name of the {@link Validable}
+     */
+    private Map<State, Map<String, GameStrategy>> table;
+    private transient static final String singlePLayerTableFile = "SinglePlayerStatesTransitionTable.json";
+    private transient static final String multiPLayerTableFile = "MultiPlayerStatesTransitionTable.json";
 
-    public static StatesTransitionTable singlePlayer() throws IOException {
-        Gson gson = new Gson();
-        String SinglePlayerStatesTransitionTable = Files.readString(Path.of("src/main/resources/config/SinglePlayerStatesTransitionTable/.json"), StandardCharsets.US_ASCII);
-        return gson.fromJson(SinglePlayerStatesTransitionTable, StatesTransitionTable.class);
+    /**
+     * Returns name of a class
+     */
+    private static String name(Class classClass){
+        return classClass.getSimpleName();
     }
 
-    public static StatesTransitionTable multiPlayer() throws IOException {
-        Gson gson = new Gson();
-        String MultiPlayerStatesTransitionTable = Files.readString(Path.of("src/main/resources/config/MultiPlayerStatesTransitionTable/.json"), StandardCharsets.US_ASCII);
-        return gson.fromJson(MultiPlayerStatesTransitionTable, StatesTransitionTable.class);
+    public static StatesTransitionTable singlePlayer() {
+        return jsonUtility.deserialize(
+                jsonUtility.configPathString+ singlePLayerTableFile,
+                StatesTransitionTable.class,
+                jsonWithAdapter()
+        );
     }
 
-    public static StatesTransitionTable fromIsSinglePlayer(boolean isSinglePlayer) throws IOException {
+    public static StatesTransitionTable multiPlayer() {
+        return jsonUtility.deserialize(
+                jsonUtility.configPathString+ multiPLayerTableFile,
+                StatesTransitionTable.class,
+                jsonWithAdapter()
+        );
+    }
+
+    /**
+     * Returns the {@link GameStrategy} to use at the given {@link State} after the given {@link Validable event}.
+     */
+    public GameStrategy getStrategy(State state, Validable event){
+        return table.get(state).get(name(event.getClass()));
+    }
+
+    /**
+     * Returns the single player table when the game is single player and the multiplayer table when it is multiplayer<br>
+     * @param isSinglePlayer a boolean indicating if the game is single player.
+     */
+    public static StatesTransitionTable fromIsSinglePlayer(boolean isSinglePlayer) {
         return isSinglePlayer?StatesTransitionTable.singlePlayer():StatesTransitionTable.multiPlayer();
     }
 
-    private void saveSinglePlayerStatesTransitionTable () throws IOException {
-
-        //table.put(State.INITIAL_PHASE,)    ;
-        //
-        //Writer writer = new FileWriter("src/main/resources/config/SinglePlayerTable.json");
-        //Gson gson = new GsonBuilder().create();
-        //gson.toJson(this, writer);
-        //writer.flush(); //flush data to file   <---
-        //writer.close(); //close write          <---
+    public static void saveTables(){
+        saveMultiPlayerStatesTransitionTable();
+        saveSinglePlayerStatesTransitionTable();
     }
 
-    private void saveMultiPlayerStatesTransitionTable() throws IOException{
+    private static void saveSinglePlayerStatesTransitionTable () {
+        StatesTransitionTable statesTransitionTable = new StatesTransitionTable();
+        statesTransitionTable.table = new HashMap<>();
 
+        HashMap<String, GameStrategy> eventsAndStrategy = new HashMap<>();
+        eventsAndStrategy.put(name(SetupPhaseEvent.class),new ChooseInitialResource());
+        statesTransitionTable.table.put(State.SETUP_PHASE,eventsAndStrategy);
+
+        eventsAndStrategy = new HashMap<>();
+        eventsAndStrategy.put(name(SkipLeaderEvent.class),new EndLeaders());
+        eventsAndStrategy.put(name(PlayLeaderEvent.class),new ActivateLeader());
+        eventsAndStrategy.put(name(DiscardLeaderEvent.class),new DiscardLeader());
+        statesTransitionTable.table.put(State.SHOWING_LEADERS_INITIAL,eventsAndStrategy);
+        //Todo add other states
+
+        jsonUtility.serialize(jsonUtility.configPathString+singlePLayerTableFile,
+                statesTransitionTable,
+                StatesTransitionTable.class,
+                jsonWithAdapter());
     }
 
-    public GameStrategy getStrategy(State state, Validable event){
-        return table.get(state).get(event);
+    private static void saveMultiPlayerStatesTransitionTable() {
+        StatesTransitionTable statesTransitionTable = new StatesTransitionTable();
+        statesTransitionTable.table = new HashMap<>();
+
+        HashMap<String, GameStrategy> eventsAndStrategy = new HashMap<>();
+        eventsAndStrategy.put(name(SetupPhaseEvent.class),new ChooseInitialResource());
+        statesTransitionTable.table.put(State.SETUP_PHASE,eventsAndStrategy);
+
+        eventsAndStrategy = new HashMap<>();
+        eventsAndStrategy.put(name(SkipLeaderEvent.class),new EndLeaders());
+        eventsAndStrategy.put(name(PlayLeaderEvent.class),new ActivateLeader());
+        eventsAndStrategy.put(name(DiscardLeaderEvent.class),new DiscardLeader());
+        statesTransitionTable.table.put(State.SHOWING_LEADERS_INITIAL,eventsAndStrategy);
+        //Todo add other states
+
+        jsonUtility.serialize(jsonUtility.configPathString+multiPLayerTableFile,
+                statesTransitionTable,
+                StatesTransitionTable.class,
+                jsonWithAdapter());
+    }
+
+    private static Gson jsonWithAdapter(){
+        RuntimeTypeAdapterFactory<GameStrategy> strategyAdapter = RuntimeTypeAdapterFactory.of(GameStrategy.class);
+
+        strategyAdapter.registerSubtype(Initial.class);
+        strategyAdapter.registerSubtype(Middle.class);
+        strategyAdapter.registerSubtype(Final.class);
+        strategyAdapter.registerSubtype(EndLeaders.class);
+        strategyAdapter.registerSubtype(DiscardLeader.class);
+        strategyAdapter.registerSubtype(ActivateLeader.class);
+        strategyAdapter.registerSubtype(ChooseInitialResource.class);
+        //Todo add all strategies
+
+        return new GsonBuilder()
+                .registerTypeAdapterFactory(strategyAdapter)
+                .create();
     }
 
 }
---
+++ src/test/java/it/polimi/ingsw/server/model/StatesTransitionTableTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.server.model;
+
+import it.polimi.ingsw.server.controller.strategy.ChooseInitialResource;
+import it.polimi.ingsw.server.messages.clienttoserver.events.setupphaseevent.SetupPhaseEvent;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StatesTransitionTableTest {
+    public StatesTransitionTable singlePlayerTable, multiPlayerTable;
+
+    @Before
+    public void setUp() throws Exception {
+        //serializeTables();
+        singlePlayerTable = StatesTransitionTable.singlePlayer();
+        multiPlayerTable = StatesTransitionTable.multiPlayer();
+    }
+
+    @Test
+    public void testSerialization(){
+        assertEquals(ChooseInitialResource.class,multiPlayerTable.getStrategy(State.SETUP_PHASE, new SetupPhaseEvent()).getClass());
+        assertEquals(ChooseInitialResource.class,singlePlayerTable.getStrategy(State.SETUP_PHASE, new SetupPhaseEvent()).getClass());
+    }
+
+    public void serializeTables(){
+        StatesTransitionTable.saveTables();
+    }
+
+}
