b93d5898b0b2b5f23a7c0ba3d62ede8f00e6c51a
calonca <alexlaconca@gmail.com>
2021-05-06 00:36:53 +0200

    Made choice inside CLI

--- src/main/java/it/polimi/ingsw/client/view/CLI/CreateMatch.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/CreateMatch.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.client.view.CLI;
 
-import it.polimi.ingsw.client.view.CLI.CLIelem.RunnableWithString;
+import it.polimi.ingsw.client.view.CLI.CLIelem.RunnableWithInputString;
 import it.polimi.ingsw.client.view.CLI.CLIelem.Spinner;
 import it.polimi.ingsw.client.view.CLI.CLIelem.Title;
 import it.polimi.ingsw.client.view.abstractview.CreateMatchViewBuilder;
@@ -19,28 +19,21 @@ public class CreateMatch extends CreateMatchViewBuilder implements CLIBuilder {
     @Override
     public void run() {
         getCLIView().setTitle(new Title("Creating match"));
-        getCLIView().displayWithDivider();
 
-        RunnableWithString rs = new RunnableWithString();
-        rs.afterInputCall(()->{
-            int numberOfPlayers;
-            do {
-                String in = rs.getString();
-                numberOfPlayers = Integer.parseInt(in);
-            }while (numberOfPlayers <1|| numberOfPlayers >4);
-
-            RunnableWithString rs2 = new RunnableWithString();
-            int finalNumberOfPlayers = numberOfPlayers;
-            rs2.afterInputCall(()->{
-                String nickName = rs2.getString();
+        Runnable r1 = ()->{
+            int numberOfPlayers = getCLIView().getLastInt();
+            Runnable r2 = ()-> {
+                String nickName = getCLIView().getLastInput();
                 getCLIView().resetCLI();
                 getCLIView().setSpinner(Spinner.matchToStart(getClient(),this));
-                getClient().getServerHandler().sendCommandMessage(new CreateMatchRequest(finalNumberOfPlayers,nickName));
+                getClient().getServerHandler().sendCommandMessage(new CreateMatchRequest(numberOfPlayers,nickName));
                 getCLIView().displayWithDivider();
-            });
-            getCLIView().getInputAndLaunchRunnable("Your nickname: ",rs2);
-        });
-        getCLIView().getInputAndLaunchRunnable("Number of people: ",rs);
+            };
+            getCLIView().runOnInput("Your nickname: ",r2);
+            getCLIView().displayWithDivider();
+        };
+        getCLIView().runOnIntInput("Number of people (1 to 4): ","Number of people not valid",1,4,r1);
+        getCLIView().displayWithDivider();
 
     }
 
