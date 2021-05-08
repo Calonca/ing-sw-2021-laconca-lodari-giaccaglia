9b7f6358ce358e353657330b5162cd30c9c80c5f
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-05-08 12:30:11 +0200

    Adding scene

--- src/main/java/it/polimi/ingsw/client/view/GUI/ConnectToServer.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/ConnectToServer.java
@@ -2,11 +2,19 @@ package it.polimi.ingsw.client.view.GUI;
 
 import it.polimi.ingsw.client.view.abstractview.ViewBuilder;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ConnectToServer extends ViewBuilder implements GUIView {
+    public StackPane connectionPane;
+    public Button connectionButton;
+    public TextField addressText;
+    public TextField portText;
+
     @Override
     public void run() {
         Stage stage = getClient().getStage();
