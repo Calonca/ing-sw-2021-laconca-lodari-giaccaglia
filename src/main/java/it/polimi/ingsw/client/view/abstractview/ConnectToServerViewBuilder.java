3915a50254a6fb7a59b99f2b5933af907d2d22a7
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-05-10 02:06:39 +0200

    Moving service method to correct location

--- src/main/java/it/polimi/ingsw/client/view/abstractview/ConnectToServerViewBuilder.java
+++ src/main/java/it/polimi/ingsw/client/view/abstractview/ConnectToServerViewBuilder.java
@@ -2,4 +2,15 @@ package it.polimi.ingsw.client.view.abstractview;
 
 public abstract class ConnectToServerViewBuilder extends ViewBuilder {
 
+    /**
+     * Using regex to verify
+     * @param ip "should" be and ip address
+     * @return true if is ip address
+     */
+    public static boolean isIPAddr(final String ip) {
+        String PATTERN = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+
+        return ip.matches(PATTERN);
+    }
+
 }
