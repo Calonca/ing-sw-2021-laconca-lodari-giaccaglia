6b99d22ba89f66ea19a5d43025c1a79ff4e2700b
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-05-21 13:02:04 +0200

    Adding initial selection model

--- src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
@@ -4,6 +4,7 @@ package it.polimi.ingsw.client.view.GUI;
 import it.polimi.ingsw.client.Client;
 
 
+import it.polimi.ingsw.server.model.Resource;
 import javafx.animation.Animation;
 import javafx.animation.FillTransition;
 import javafx.animation.TranslateTransition;
@@ -27,7 +28,9 @@ import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -35,19 +38,27 @@ import java.util.ResourceBundle;
  */
 public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupPhaseViewBuilder implements GUIView {
 
-    public Button b1;
-    public Button b2;
-    public Button b3;
-    public Button b4;
-    public Button confirm;
+    @FXML
+    private Button b1;
+    @FXML
+    private Button b2;
+    @FXML
+    private Button b3;
+    @FXML
+    private Button b4;
+    @FXML
+    private Button confirm;
+    List<Button> sceneButtons=new ArrayList<Button>();
+
 
     boolean[] selected =new boolean[4];
     Spinner<Integer> goldSpin;
     Spinner<Integer> slaveSpin;
     Spinner<Integer> stoneSpin;
     Spinner<Integer> shieldSpin;
-
-    public AnchorPane cjlAnchor;
+    List<Spinner<Integer>> sceneSpinners=new ArrayList<Spinner<Integer>>();
+    @FXML
+    private AnchorPane cjlAnchor;
 
 
     @Override
@@ -67,139 +78,116 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
         getClient().getStage().show();
     }
 
+    public Spinner<Integer> colorAndAnimate(String color ,int height, int x)
+    {
+        Spinner<Integer> spin=new Spinner<>(0,3,0);
+        spin.setPadding(new Insets(10,10,10,10));
+        spin.setBackground(Background.EMPTY);
+        spin.styleProperty().set(color);
+        spin.setLayoutY(height);
+        spin.setLayoutX(x);
+        spin.setOnMouseClicked( p ->
+        {
+            Circle circle2=new Circle();
+            circle2.setRadius(10);
+            circle2.setLayoutX(x);
+            circle2.setLayoutY(height);
+
+            FillTransition filltransition2= new FillTransition(Duration.seconds(1.5),circle2,Color.GOLD,Color.PURPLE);
+            filltransition2.setCycleCount(Animation.INDEFINITE);
+            filltransition2.setAutoReverse(true);
+            filltransition2.play();
+
+            TranslateTransition transition2 = new TranslateTransition(Duration.seconds(1.5),circle2);
+            transition2.setToX(0);
+            transition2.setToY(-200);
+            transition2.setAutoReverse(false);
+            transition2.play();
+            cjlAnchor.getChildren().add(circle2);
+
+            Client.getInstance().getStage().show();
+        });
+
+        return spin;
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         int height=200;
         int x=120;
         Arrays.fill(selected, false);
-        goldSpin=new Spinner<>(0,3,0);
-        goldSpin.setPadding(new Insets(10,10,10,10));
-        goldSpin.setBackground(Background.EMPTY);
-        goldSpin.styleProperty().set(" -fx-background-color: #B8860B");
-        goldSpin.setLayoutY(height);
-        goldSpin.setLayoutX(x);
+
+
+        goldSpin=colorAndAnimate(" -fx-background-color: #B8860B",height,x);
         cjlAnchor.getChildren().add(goldSpin);
 
-        goldSpin.setOnMouseClicked( p ->
-        {
-                Circle circle2=new Circle();
-        circle2.setRadius(10);
-        circle2.setLayoutX(120);
-        circle2.setLayoutY(200);
-
-        FillTransition filltransition2= new FillTransition(Duration.seconds(1.5),circle2,Color.GOLD,Color.PURPLE);
-        filltransition2.setCycleCount(Animation.INDEFINITE);
-        filltransition2.setAutoReverse(true);
-        filltransition2.play();
-
-        TranslateTransition transition2 = new TranslateTransition(Duration.seconds(1.5),circle2);
-        transition2.setToX(0);
-        transition2.setToY(-200);
-        transition2.setAutoReverse(false);
-        transition2.play();
-        cjlAnchor.getChildren().add(circle2);
-
-        Client.getInstance().getStage().show();
-        });
 
         x+=200;
-        slaveSpin=new Spinner<>(0,3,0);
-        slaveSpin.setPadding(new Insets(10,10,10,10));
-        slaveSpin.setBackground(Background.EMPTY);
-        slaveSpin.styleProperty().set(" -fx-background-color: #9400D3");
-        slaveSpin.setLayoutY(height);
-        slaveSpin.setLayoutX(x);
+        slaveSpin=colorAndAnimate("  -fx-background-color: #0099FF",height,x);
         cjlAnchor.getChildren().add(slaveSpin);
 
+
         x+=200;
-        shieldSpin=new Spinner<>(0,3,0);
-        shieldSpin.setPadding(new Insets(10,10,10,10));
-        shieldSpin.setBackground(Background.EMPTY);
-        shieldSpin.styleProperty().set(" -fx-background-color: #0099FF");
-        shieldSpin.setLayoutY(height);
-        shieldSpin.setLayoutX(x);
+        shieldSpin=colorAndAnimate(" -fx-background-color: #DEB887",height,x);
         cjlAnchor.getChildren().add(shieldSpin);
 
         x+=200;
-        stoneSpin=new Spinner<>(0,3,0);
-        stoneSpin.setPadding(new Insets(10,10,10,10));
-        stoneSpin.setBackground(Background.EMPTY);
-        stoneSpin.styleProperty().set(" -fx-background-color: #DEB887");
-        stoneSpin.setLayoutY(height);
-        stoneSpin.setLayoutX(x);
+        stoneSpin=colorAndAnimate(" -fx-background-color: #9400D3",height,x);
         cjlAnchor.getChildren().add(stoneSpin);
+
+        sceneSpinners.add(goldSpin);
+        sceneSpinners.add((slaveSpin));
+        sceneSpinners.add(shieldSpin);
+        sceneSpinners.add(stoneSpin);
 
         ImageView temp;
 
-        temp=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
-        temp.setFitHeight(200);
-        temp.setFitWidth(200);
-        b1.setGraphic(temp);
-        temp=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
-        temp.setFitHeight(200);
-        temp.setFitWidth(200);
-        b2.setGraphic(temp);
-        temp=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
-        temp.setFitHeight(200);
-        temp.setFitWidth(200);
-        b3.setGraphic(temp);
-        temp=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
-        temp.setFitHeight(200);
-        temp.setFitWidth(200);
-        b4.setOnAction(e ->
-        {
-            ImageView tempo=new ImageView(new Image("assets/leaders/grayed out/BACK/Masters of Renaissance__Cards_BACK.png", true));
-            tempo.setFitHeight(200);
-            tempo.setFitWidth(200);
-            b4.setGraphic(tempo);
-        });
-        b4.setGraphic(temp);
+        sceneButtons.add(b1);
+        sceneButtons.add(b2);
+        sceneButtons.add(b3);
+        sceneButtons.add(b4);
 
-        b1.setOnAction(e ->
-        {
-            ImageView tempo;
-            if(!selected[1])
+        for (Button sceneButton : sceneButtons) {
+            temp = new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
+            temp.setFitHeight(200);
+            temp.setFitWidth(200);
+            sceneButton.setGraphic(temp);
+            sceneButton.setOnAction(e ->
             {
-                tempo=new ImageView(new Image("assets/leaders/grayed out/BACK/Masters of Renaissance__Cards_BACK.png", true));
-                tempo.setFitHeight(200);
-                tempo.setFitWidth(200);
-                b1.setGraphic(tempo);
-                selected[1]=true;
-            }
-            else
-            {
-                tempo=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
-                tempo.setFitHeight(200);
-                tempo.setFitWidth(200);
-                b1.setGraphic(tempo);
-                selected[1]=false;
-            }
+                ImageView tempo;
+                if(!selected[sceneButtons.indexOf(sceneButton)])
+                {
+                    tempo=new ImageView(new Image("assets/leaders/grayed out/BACK/Masters of Renaissance__Cards_BACK.png", true));
+                    tempo.setFitHeight(200);
+                    tempo.setFitWidth(200);
+                    sceneButton.setGraphic(tempo);
+                    selected[sceneButtons.indexOf(sceneButton)]=true;
+                }
+                else
+                {
+                    tempo=new ImageView(new Image("assets/leaders/raw/FRONT/Masters of Renaissance_Cards_FRONT_0.png", true));
+                    tempo.setFitHeight(200);
+                    tempo.setFitWidth(200);
+                    sceneButton.setGraphic(tempo);
+                    selected[sceneButtons.indexOf(sceneButton)]=false;
+                }
 
-        });
+            });
 
-        b2.setOnAction(e ->
-        {
-            ImageView tempo=new ImageView(new Image("assets/leaders/grayed out/BACK/Masters of Renaissance__Cards_BACK.png", true));
-            tempo.setFitHeight(200);
-            tempo.setFitWidth(200);
-            b2.setGraphic(tempo);
-        });
+        }
 
-        b3.setOnAction(e ->
-        {
-            ImageView tempo=new ImageView(new Image("assets/leaders/grayed out/BACK/Masters of Renaissance__Cards_BACK.png", true));
-            tempo.setFitHeight(200);
-            tempo.setFitWidth(200);
-            b3.setGraphic(tempo);
-        });
 
         Client.getInstance().getStage().show();
 
     }
 
     public void handleButton(){
-        Client.getInstance().changeViewBuilder(new ViewPersonalBoard());
+
+        for(int i=0;i<sceneSpinners.size();i++)
+            System.out.println(sceneSpinners.get(i).getValue() + Resource.fromInt(i).toString());
+        for(int i=0;i<sceneButtons.size();i++)
+            System.out.println((selected[i]+ sceneButtons.get(i).toString()));
+        //Client.getInstance().changeViewBuilder(new ViewPersonalBoard());
 
     }
 
