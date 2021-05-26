fad746974610c3701154b75f9cf78faf82183091
calonca <alexlaconca@gmail.com>
2021-05-26 14:00:11 +0200

    Remove unused method

--- src/main/java/it/polimi/ingsw/network/simplemodel/TestElem.java
+++ src/main/java/it/polimi/ingsw/network/simplemodel/TestElem.java
@@ -1,10 +1,6 @@
 package it.polimi.ingsw.network.simplemodel;
 
 public class TestElem extends SimpleModelElement{
-    @Override
-    public Object getElement() {
-        return null;
-    }
 
     @Override
     public void update(SimpleModelElement element) {
