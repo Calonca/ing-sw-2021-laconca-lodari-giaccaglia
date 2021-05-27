444c9aa2eee9f54e4a31c74f8df0a44685c553b8
calonca <alexlaconca@gmail.com>
2021-05-27 15:31:03 +0200

    Horizontal listview working

--- src/main/java/it/polimi/ingsw/client/view/CLI/CLIelem/body/HorizontalListBody.java
+++ src/main/java/it/polimi/ingsw/client/view/CLI/CLIelem/body/HorizontalListBody.java
@@ -1,36 +1,96 @@
 package it.polimi.ingsw.client.view.CLI.CLIelem.body;
 
 import it.polimi.ingsw.client.view.CLI.CLI;
+import it.polimi.ingsw.client.view.CLI.CLIelem.Option;
 import it.polimi.ingsw.client.view.CLI.CLIelem.OptionList;
-import it.polimi.ingsw.client.view.CLI.textUtil.Canvas;
-import it.polimi.ingsw.client.view.CLI.textUtil.StringUtil;
-import org.apache.commons.lang3.StringUtils;
+import it.polimi.ingsw.client.view.CLI.textUtil.*;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class HorizontalListBody extends OptionList {
+
     int height;
+    Mode mode;
+
+    public enum Mode {
+        WIDE{
+            @Override
+            public String s(HorizontalListBody horizontalListBody) {
+                return horizontalListBody.toWideString();
+            }
+        }
+        ,COMPACT {
+            @Override
+            public String s(HorizontalListBody horizontalListBody) {
+                return horizontalListBody.toCenteredString();
+            }
+        };
+        public abstract String s(HorizontalListBody horizontalListBody);
+
+    }
+
+    public HorizontalListBody(int height) {
+        this.height = height;
+        this.mode = Mode.COMPACT;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
 
     @Override
     public String toString() {
+        return mode.s(this);
+    }
+
+    public String toWideString() {
+        int width = CLI.width;
+        int optWidth = width/options.size();
+        AtomicInteger startX = new AtomicInteger(optWidth / options.size());
         Canvas canvas = Canvas.withBorder(CLI.width,cli.getMaxBodyHeight());
-        //canvas.drawWithDefaultColor(spaces.length(),height,);
+        toStringStream().forEach(o->{
+            canvas.drawShifted(startX.get(),(height-optMaxHeight())/2,o);
+            startX.addAndGet(optWidth);
+        });
         return canvas.toString();
-        //Todo make spaces center the object.
-        //String spaces = "                                                    ";
-        //return toStringStream()
-        //        .map(s -> spaces +s.replace("\n","\n"+spaces)).reduce("",(a,b)->a+b);
     }
 
-    public Stream<String> toStringStream() {
+
+    public String toCenteredString() {
+        int width = CLI.width;
+        int optWidth = optMaxWidth()+4;
+        AtomicInteger startX = new AtomicInteger((width-(optWidth*options.size()))/2);
+        Canvas canvas = Canvas.withBorder(CLI.width,cli.getMaxBodyHeight());
+        toStringStream().forEach(o->{
+            canvas.drawShifted(startX.get(),(height-optMaxHeight())/2,o);
+            startX.addAndGet(optWidth);
+        });
+        return canvas.toString();
+    }
+
+
+    public Stream<DrawableList> toStringStream() {
+        int spaces = (optMaxWidth()/2)-2;
         return IntStream.range(0,options.size())
-                .mapToObj(i->i+": "+options.get(i).toString());
+                .mapToObj(i->{
+                    DrawableList drawableList = new DrawableList();
+                    drawableList.add(new Drawable(spaces,0, i+":", Color.ANSI_BLUE,Background.DEFAULT));
+                    DrawableList drawableOptions = DrawableList.shifted(0,1,options.get(i).toDrawableList());
+                    drawableList.add(drawableOptions);
+                    return drawableList;});
     }
 
-    private int verticalSize()
+    private int optMaxWidth()
     {
-        return options.stream().mapToInt(o->StringUtil.maxHeight(o.toString())).max().orElse(0);
+        return options.stream().mapToInt(Option::horizontalSize).max().orElse(0)+1;
+    }
+
+    private int optMaxHeight()
+    {
+        return options.stream().mapToInt(Option::height).max().orElse(0);
     }
 
 
---
+++ src/main/java/it/polimi/ingsw/client/view/CLI/textUtil/Drawable.java
@@ -0,0 +1,80 @@
+package it.polimi.ingsw.client.view.CLI.textUtil;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Drawable {
+    private Color color;
+    private Background background;
+    private String string;
+    private int XPos;
+    private int YPos;
+    private int height;
+    private int width;
+
+    public Drawable(int XPos, int YPos, String string, Color color, Background background) {
+        this.color = color;
+        this.background = background;
+        this.string = string;
+        this.XPos = XPos;
+        this.YPos = YPos;
+        setHeightAndWidth(string);
+    }
+
+    public Drawable(int XPos, int YPos, String string) {
+        this.string = string;
+        this.XPos = XPos;
+        this.YPos = YPos;
+        this.color = Color.DEFAULT;
+        this.background = Background.DEFAULT;
+        setHeightAndWidth(string);
+    }
+
+    public static Drawable shifted(int shiftX, int shiftY, Drawable d)
+    {
+        int XPos=d.XPos+shiftX;
+        int YPos=d.YPos+shiftY;
+        return new Drawable(XPos,YPos,d.string,d.color,d.background);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setBackground(Background background) {
+        this.background = background;
+    }
+
+    private void setHeightAndWidth(String s){
+        height = StringUtil.maxHeight(s);
+        width = StringUtil.maxWidth(s);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Background getBackground() {
+        return background;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public int getXPos() {
+        return XPos;
+    }
+
+    public int getYPos() {
+        return YPos;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
