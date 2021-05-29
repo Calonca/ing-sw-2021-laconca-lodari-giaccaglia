e74ea873c8a5e8fa7e313687e250f827c18c0115
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-05-29 23:16:21 +0200

    Adding buttons with animation and testing movement

--- src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
@@ -5,35 +5,24 @@ import it.polimi.ingsw.client.Client;
 
 
 import it.polimi.ingsw.client.json.Deserializator;
+import it.polimi.ingsw.client.view.GUI.GUIelem.ButtonSelectionModel;
+import it.polimi.ingsw.client.view.GUI.GUIelem.ResourceButton;
+import it.polimi.ingsw.client.view.GUI.GUIelem.ResourceSphere;
 import it.polimi.ingsw.network.assets.CardAssetsContainer;
 import it.polimi.ingsw.network.assets.LeaderCardAsset;
 import it.polimi.ingsw.network.simplemodel.SimplePlayerLeaders;
 import it.polimi.ingsw.network.util.Util;
 import it.polimi.ingsw.server.model.Resource;
-import javafx.animation.Animation;
-import javafx.animation.FillTransition;
-import javafx.animation.TranslateTransition;
-import javafx.beans.InvalidationListener;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
-import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.beans.PropertyChangeEvent;
 import java.io.IOException;
 import java.net.URL;
@@ -48,10 +37,16 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
      * By changin LEADERNUMBER the number of received leaders to show can be easily changed
      */
     private final int LEADERNUMBER=4;
-    List<Button> sceneButtons=new ArrayList<>();
+
+    List<Button> scenesLeadersToChoose=new ArrayList<>();
+    List<Button> sceneResources=new ArrayList<>();
+
     javafx.collections.ObservableList<Boolean> selected;
+    javafx.collections.ObservableList<Integer> selectedRes;
+
+    List<Color> colors=new ArrayList<>();
     List<String> colorsToRes=new ArrayList<>();
-    List<Spinner<Integer>> sceneSpinners= new ArrayList<>();
+
     int resourcesToChoose = Util.resourcesToChooseOnSetup(getCommonData().getThisPlayerIndex());
 
     @FXML
@@ -84,36 +79,11 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
     /**
      *
      * @param selected is a boolean array used to represent selection
-     * @param sceneButtons is a button array for the user to execute selection
+     * @param scenesLeadersToChoose is a button array for the user to execute selection
      */
-    public void bindForSelection(List<Boolean> selected,List<Button> sceneButtons,int maxselection)
-    {
-
-        //SimplePlayerLeaders simplePlayerLeaders = getThisPlayerCache().getElem(SimplePlayerLeaders.class).get();
-        //List<LeaderCardAsset> leaderpics=simplePlayerLeaders.getPlayerLeaders();
 
 
-        for (Button sceneButton : sceneButtons) {
 
-
-        //sceneButton.setGraphic(temp);
-        sceneButton.setOnAction(e ->
-        {
-
-            {
-                if(!selected.get(sceneButtons.indexOf(sceneButton)))
-                {
-                    selected.set(sceneButtons.indexOf(sceneButton),true);
-
-                }
-                else
-                {
-                    selected.set(sceneButtons.indexOf(sceneButton),false);
-                }
-            }
-        });
-
-    }}
 
     /**
      * given a color and a position, the method will build one accordingly
@@ -122,38 +92,6 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
      * @param x is an int position
      * @return a spinner according to parameters
      */
-    public Spinner<Integer> colorAndAnimate(String color ,int y, int x)
-    {
-        Spinner<Integer> spin=new Spinner<>(0,3,0);
-        spin.setPadding(new Insets(10,10,10,10));
-        spin.setBackground(Background.EMPTY);
-        spin.styleProperty().set(color);
-        spin.setLayoutY(y);
-        spin.setLayoutX(x);
-        spin.setOnMouseClicked( p ->
-        {
-            Circle circle2=new Circle();
-            circle2.setRadius(10);
-            circle2.setLayoutX(x);
-            circle2.setLayoutY(y);
-
-            FillTransition filltransition2= new FillTransition(Duration.seconds(1.5),circle2,Color.GOLD,Color.PURPLE);
-            filltransition2.setCycleCount(Animation.INDEFINITE);
-            filltransition2.setAutoReverse(true);
-            filltransition2.play();
-
-            TranslateTransition transition2 = new TranslateTransition(Duration.seconds(1.5),circle2);
-            transition2.setToX(0);
-            transition2.setToY(-200);
-            transition2.setAutoReverse(false);
-            transition2.play();
-            cjlAnchor.getChildren().add(circle2);
-
-            Client.getInstance().getStage().show();
-        });
-
-        return spin;
-    }
 
     /**
      * service method
@@ -167,10 +105,10 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
         confirm.setLayoutX(450);
         confirm.setOnAction(p -> {
 
-            for(int i=0;i<sceneSpinners.size();i++)
-                System.out.println(sceneSpinners.get(i).getValue() + Resource.fromInt(i).toString());
-            for(int i=0;i<sceneButtons.size();i++)
-                System.out.println((selected.get(i)+ sceneButtons.get(i).toString()));
+            for(int i=0;i<sceneResources.size();i++)
+                System.out.println(selectedRes.get(i) + Resource.fromInt(i).toString());
+            for(int i=0;i<scenesLeadersToChoose.size();i++)
+                System.out.println((selected.get(i)+ scenesLeadersToChoose.get(i).toString()));
             Client.getInstance().changeViewBuilder(new MarketMatrix());
 
         });
@@ -180,25 +118,25 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        ButtonSelectionModel selectionModel=new ButtonSelectionModel();
         int y=200;
         int x=120;
 
         colorsToRes.add(" -fx-background-color: #B8860B");
+        colorsToRes.add(" -fx-background-color: #9400D3");
         colorsToRes.add(" -fx-background-color: #0099FF");
         colorsToRes.add(" -fx-background-color: #DEB887");
-        colorsToRes.add(" -fx-background-color: #9400D3");
 
-        /**
-         * Spinner are built accordig to resource colors
-         */
-        //TODO ADD GETCOLOR TO RESOURCE
-        Spinner<Integer> colorToResSpin;
-        for (String colorsToRe : colorsToRes) {
-            colorToResSpin = colorAndAnimate(colorsToRe, y, x);
-            cjlAnchor.getChildren().add(colorToResSpin);
-            sceneSpinners.add(colorToResSpin);
-            x += 200;
-        }
+
+        Label resNumber=new Label(Integer.toString(Util.resourcesToChooseOnSetup(getCommonData().getThisPlayerIndex())));
+        resNumber.setLayoutX(300);
+        resNumber.setLayoutY(200);
+        cjlAnchor.getChildren().add(resNumber);
+
+        Label leaders=new Label("SELEZIONA DUE LEADER");
+        leaders.setLayoutX(300);
+        leaders.setLayoutY(500);
+        cjlAnchor.getChildren().add(leaders);
 
 
         /**
@@ -218,38 +156,77 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
             tempbut.setLayoutY(600);
             tempbut.setLayoutX(100+200*i);
             tempbut.setGraphic(temp);
+
             temp.setFitHeight(200);
             temp.setFitWidth(200);
             cjlAnchor.getChildren().add(tempbut);
-            sceneButtons.add(tempbut);
+            scenesLeadersToChoose.add(tempbut);
 
         }
 
 
         selected=javafx.collections.FXCollections.observableArrayList();
-
-
-
         for (int i=0;i<LEADERNUMBER;i++)
             selected.add(false);
 
 
-       bindForSelection(selected,sceneButtons,2);
+        selectionModel.bindForSelection(selected,scenesLeadersToChoose,2);
 
         selected.addListener(new javafx.collections.ListChangeListener<Boolean>() {
             @Override
             public void onChanged(Change<? extends Boolean> c) {
                 c.next();
                 if(c.getAddedSubList().get(0))
-                    sceneButtons.get(c.getFrom()).setLayoutY(sceneButtons.get(c.getFrom()).getLayoutY()-30);
+                    scenesLeadersToChoose.get(c.getFrom()).setLayoutY(scenesLeadersToChoose.get(c.getFrom()).getLayoutY()-30);
                 else
-                    sceneButtons.get(c.getFrom()).setLayoutY(sceneButtons.get(c.getFrom()).getLayoutY()+30);
+                    scenesLeadersToChoose.get(c.getFrom()).setLayoutY(scenesLeadersToChoose.get(c.getFrom()).getLayoutY()+30);
 
 
             }});
 
-       cjlAnchor.getChildren().add(validationButton());
-       Client.getInstance().getStage().show();
+        selectedRes=javafx.collections.FXCollections.observableArrayList();
+        for (int i=0;i<4;i++)
+            selectedRes.add(0);
+
+
+        colors.add(Color.GOLD);
+        colors.add(Color.PURPLE);
+        colors.add(Color.BLUE);
+        colors.add(Color.GREY);
+
+        ResourceButton resbut;
+        for(int i=0;i<4;i++)
+        {
+            resbut= new ResourceButton();
+            resbut.setLayoutY(200);
+            resbut.setLayoutX(100+200*i);
+            resbut.setResource(Resource.fromInt(i));
+
+
+            resbut.color();
+            cjlAnchor.getChildren().add(resbut);
+            sceneResources.add(resbut);
+
+        }
+
+        selectionModel.bindForTaking(selectedRes,sceneResources,4);
+
+        selectedRes.addListener(new javafx.collections.ListChangeListener<Integer>() {
+            @Override
+            public void onChanged(Change<? extends Integer> c) {
+                c.next();
+                Circle temp=new Circle();
+                temp.setLayoutX(430+25*selectedRes.stream().mapToInt(i -> i).sum());
+                temp.setLayoutY(400);
+                temp.setRadius(20);
+                temp.setFill(colors.get(c.getFrom()));
+                cjlAnchor.getChildren().add(temp);
+                Client.getInstance().getStage().show();
+
+
+            }});
+        cjlAnchor.getChildren().add(validationButton());
+        Client.getInstance().getStage().show();
 
     }
 
--- src/main/java/it/polimi/ingsw/client/view/GUI/ViewPersonalBoard.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/ViewPersonalBoard.java
@@ -1,25 +1,36 @@
 package it.polimi.ingsw.client.view.GUI;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.view.GUI.GUIelem.ButtonSelectionModel;
+import it.polimi.ingsw.client.view.GUI.GUIelem.ResourceButton;
+import it.polimi.ingsw.server.model.Resource;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 import java.beans.PropertyChangeEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ViewPersonalBoard extends it.polimi.ingsw.client.view.abstractview.SetupPhaseViewBuilder implements GUIView
 {
-    public StackPane menuPane;
-    public ImageView boardView;
+    public AnchorPane menuPane;
+
+    List<ResourceButton> sceneResources=new ArrayList<>();
+    List<Boolean> selected=new ArrayList<>();
+
 
     @Override
     public void run() {
@@ -52,19 +63,53 @@ public class ViewPersonalBoard extends it.polimi.ingsw.client.view.abstractview.
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        ImageView tempo;
-        tempo=new ImageView(new Image("assets/board/smallerboard.png", true));
+        ImageView tempImageView;
+        tempImageView=new ImageView(new Image("assets/board/smallerboard.png", true));
 
-        tempo.setFitHeight(511);
-        tempo.setFitWidth(530);
-        menuPane.getChildren().add(new Label("si nu topy"));
-
+        tempImageView.setFitHeight(511);
+        tempImageView.setFitWidth(530);
        /* boardView.setImage(new Image("assets/board/Masters of Renaissance_PlayerBoard (11_2020)-1.png", true));
         boardView.setRotate(270);
         boardView.setFitHeight(600);
         boardView.setFitWidth(9000);*/
-        StackPane.setAlignment(tempo, Pos.BOTTOM_CENTER);
-        menuPane.getChildren().add(0,tempo);
+        StackPane.setAlignment(tempImageView, Pos.BOTTOM_CENTER);
+        //menuPane.getChildren().add(0,tempImageView);
+        List<Color> colors=new ArrayList<>();
+        List<String> colorsToRes=new ArrayList<>();
+
+        for(int i=0;i<4;i++)
+        {
+            ResourceButton tempbut= new ResourceButton();
+            tempbut.setResource(Resource.fromInt(i));
+            tempbut.setLayoutY(200);
+            tempbut.setLayoutX(100+200*i);
+            tempbut.color();
+
+            //tempbut.setGraphic(buttonGraphic);
+            menuPane.getChildren().add(tempbut);
+            sceneResources.add(tempbut);
+
+        }
+
+        for(int i=0;i<4;i++)
+        {
+            ResourceButton tempbut= new ResourceButton();
+            tempbut.setResource(Resource.EMPTY);
+            tempbut.setLayoutY(300);
+            tempbut.setLayoutX(100+200*i);
+            //tempbut.setGraphic(buttonGraphic);
+            menuPane.getChildren().add(tempbut);
+            sceneResources.add(tempbut);
+
+        }
+        for(int i=0;i<sceneResources.size();i++)
+            selected.add(false);
+        ButtonSelectionModel selectionModel=new ButtonSelectionModel();
+        selectionModel.initializeFalseOnEmpty(selected,sceneResources);
+        selectionModel.disableFalseEnableTrue(selected,sceneResources);
+        selectionModel.bindToMove(selected,sceneResources);
+
+
         Client.getInstance().getStage().show();
     }
 }
