103e8f3d8465a14c4aead2b7169bc439b57a5864
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-06-07 00:27:23 +0200

    Adding MiddlePhase

--- src/main/java/it/polimi/ingsw/client/view/GUI/MiddlePhaseGUI.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/MiddlePhaseGUI.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.view.abstractview.MiddlePhaseViewBuilder;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -23,13 +24,16 @@ public class MiddlePhaseGUI extends MiddlePhaseViewBuilder implements GUIView {
 
     @Override
     public void run() {
-        ((Pane) getClient().getStage().getScene().getRoot()).getChildren().remove(0);
-        ((Pane)getClient().getStage().getScene().getRoot()).getChildren().add(getRoot());
+        SubScene toadd=getRoot();
+        toadd.setId("MIDDLE");
+        ((Pane)getClient().getStage().getScene().getRoot()).getChildren().add(toadd);
+        System.out.println(((Pane)getClient().getStage().getScene().getRoot()).getChildren());
+
     }
 
-    public Parent getRoot() {
+    public SubScene getRoot() {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/fxml/SetupMenu.fxml"));
+        loader.setLocation(getClass().getResource("/fxml/MiddlePhase.fxml"));
         Parent root = null;
         try {
             root = loader.load();
@@ -38,7 +42,7 @@ public class MiddlePhaseGUI extends MiddlePhaseViewBuilder implements GUIView {
             e.printStackTrace();
         }
 
-        return root;
+        return new SubScene(root,500,400);
 
     }
 
@@ -47,24 +51,22 @@ public class MiddlePhaseGUI extends MiddlePhaseViewBuilder implements GUIView {
 
     }
 
+    public void sendChoice(Choice choice) {
+
+        sendMessage(choice);
+        ((Pane)getClient().getStage().getScene().getRoot()).getChildren().remove(4);
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Todo use MiddlePhaseViewBuilder.sendMessage(Choice.PRODUCTION);
         // MiddlePhaseViewBuilder.sendMessage(Choice.RESOURCE_MARKET);
         // MiddlePhaseViewBuilder.sendMessage(Choice.CARD_SHOP);
         // to send the messages to the client
-        cardButton.setOnAction( e -> sendMessage(Choice.CARD_SHOP));
-        productionButton.setOnAction( e -> sendMessage(Choice.PRODUCTION));
-        resourceMarketButton.setOnAction( e -> sendMessage(Choice.RESOURCE_MARKET));
-        ImageView temp = new ImageView(new Image("assets/punchboard/MarketBoard.png", true));
-        temp.setFitWidth(100);
-        resourceMarketButton.setGraphic(temp);
-        temp = new ImageView(new Image("assets/devCards/raw/FRONT/Masters of Renaissance_Cards_FRONT_BLUE_1.png", true));
-        temp.setFitWidth(100);
-        cardButton.setGraphic(temp);
-        temp = new ImageView(new Image("assets/board/smallerboard.png", true));
-        temp.setFitWidth(100);
-        productionButton.setGraphic(temp);
+        cardButton.setOnAction( e -> sendChoice(Choice.CARD_SHOP));
+        productionButton.setOnAction( e -> sendChoice(Choice.PRODUCTION));
+        resourceMarketButton.setOnAction( e -> sendChoice(Choice.RESOURCE_MARKET));
+
 
     }
 }
