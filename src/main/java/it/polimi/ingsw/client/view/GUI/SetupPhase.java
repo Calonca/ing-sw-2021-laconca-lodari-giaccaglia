cbb6f938124a193fa85936505bdc541c2bbced77
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-06-13 16:37:27 +0200

    Removing unnecessary buttons from setup and initial, fixing middle logic error

--- src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/SetupPhase.java
@@ -1,23 +1,18 @@
 package it.polimi.ingsw.client.view.GUI;
 
 
-import it.polimi.ingsw.client.simplemodel.State;
-import it.polimi.ingsw.client.view.GUI.GUIelem.ButtonSelectionModel;
 import it.polimi.ingsw.network.assets.LeaderCardAsset;
 import it.polimi.ingsw.network.messages.clienttoserver.events.EventMessage;
 import it.polimi.ingsw.network.messages.clienttoserver.events.setupphaseevent.SetupPhaseEvent;
 import it.polimi.ingsw.network.simplemodel.SimplePlayerLeaders;
 import it.polimi.ingsw.network.util.Util;
-import it.polimi.ingsw.server.model.Resource;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.SubScene;
 import javafx.scene.control.*;
@@ -27,7 +22,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.util.Duration;
 import javafx.util.Pair;
-import java.beans.PropertyChangeEvent;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
@@ -43,8 +38,10 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
 
     List<UUID> leadersUUIDs=new ArrayList<>();
 
-    List<Button> sceneLeaders =new ArrayList<>();
     List<Button> sceneResources=new ArrayList<>();
+
+    List<ImageView> resourceImageViews=new ArrayList<>();
+    List<ImageView> leaderImageViews =new ArrayList<>();
 
     javafx.collections.ObservableList<Boolean> selectedLeaders;
     javafx.collections.ObservableList<Integer> selectedResources;
@@ -69,12 +66,14 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
     @Override
     public void run() {
         SubScene root=getRoot();
-    /*    root.translateYProperty().set(getClient().getStage().getScene().getHeight());
+
+        root.translateYProperty().set(GUI.getRealPane().getHeight());
         Timeline timeline=new Timeline();
         KeyValue kv= new KeyValue(root.translateYProperty(),0, Interpolator.EASE_IN);
         KeyFrame kf= new KeyFrame(Duration.seconds(0.5),kv);
         timeline.getKeyFrames().add(kf);
-        timeline.play();*/
+        timeline.play();
+        
         root.setId("SETUP");
         GUI.getRealPane().getChildren().remove(0);
         GUI.getRealPane().getChildren().add(root);
@@ -130,7 +129,7 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
 
             SetupPhaseEvent event = new SetupPhaseEvent(Util.resourcesToChooseOnSetup(getCommonData().getThisPlayerIndex()),2,getClient().getCommonData().getThisPlayerIndex());
 
-            for(int i = 0; i< sceneLeaders.size(); i++)
+            for(int i = 0; i< leaderImageViews.size(); i++)
                 if(selectedLeaders.get(i))
                     event.addChosenLeader(leadersUUIDs.get(i));
 
@@ -180,74 +179,66 @@ public class SetupPhase extends  it.polimi.ingsw.client.view.abstractview.SetupP
         leaders.setLayoutY(len/2);
         setupAnchor.getChildren().add(leaders);
 
-        Button leaderBut;
+        ImageView leaderBut;
         for(int i=0;i<getSetupLeaderIcons().size();i++)
         {
-            leaderBut= new Button();
-            leaderBut.setEffect(SetupPhase.this.effect);
-            leaderBut.setLayoutY(len-leaderSize-10);
+            leaderBut=getSetupLeaderIcons().get(i);
             leaderBut.setLayoutX(leadersOffsetX+leadersSpacing*i);
-            leaderBut.setGraphic(getSetupLeaderIcons().get(i));
+            leaderBut.setLayoutY(len-leaderSize-10);
             leaderBut.setId("leaderButton");
             leaderBut.setRotate(Math.random() * (cardTilt - -cardTilt + 1) + -1 );
             setupAnchor.getChildren().add(leaderBut);
-            sceneLeaders.add(leaderBut);
+            leaderImageViews.add(leaderBut);
         }
 
 
         selectedLeaders=javafx.collections.FXCollections.observableArrayList();
-        for (int i=0;i<LEADERNUMBER;i++)
+        for (int i=0;i<getSetupLeaderIcons().size();i++)
             selectedLeaders.add(false);
 
 
-        ViewPersonalBoard.getController().cardSelector(selectedLeaders, sceneLeaders,2);
-
+        //ViewPersonalBoard.getController().cardSelector(selectedLeaders, sceneLeaders,2);
+        ViewPersonalBoard.getController().cardSelectorFromImage(selectedLeaders, leaderImageViews,2);
         selectedLeaders.addListener((ListChangeListener<Boolean>) c -> {
             c.next();
             if(c.getAddedSubList().get(0))
-                sceneLeaders.get(c.getFrom()).setLayoutY(sceneLeaders.get(c.getFrom()).getLayoutY()-30);
+                leaderImageViews.get(c.getFrom()).setLayoutY(leaderImageViews.get(c.getFrom()).getLayoutY()-30);
             else
-                sceneLeaders.get(c.getFrom()).setLayoutY(sceneLeaders.get(c.getFrom()).getLayoutY()+30);
+                leaderImageViews.get(c.getFrom()).setLayoutY(leaderImageViews.get(c.getFrom()).getLayoutY()+30);
 
 
         });
 
         selectedResources =javafx.collections.FXCollections.observableArrayList();
 
-        List<Image> res=new ArrayList<>();
-        res.add(new Image("assets/resources/GOLD.png"));
-        res.add(new Image("assets/resources/SERVANT.png"));
-        res.add(new Image("assets/resources/SHIELD.png"));
-        res.add(new Image("assets/resources/STONE.png"));
+        resourceImageViews=new ArrayList<>();
+        resourceImageViews.add(new ImageView(new Image("assets/resources/GOLD.png")));
+        resourceImageViews.add(new ImageView(new Image("assets/resources/SERVANT.png")));
+        resourceImageViews.add(new ImageView(new Image("assets/resources/SHIELD.png")));
+        resourceImageViews.add(new ImageView(new Image("assets/resources/STONE.png")));
+
 
         Button resbut;
-        for(int i=0;i<4;i++)
+        for(int i=0;i<resourceImageViews.size();i++)
         {
-            resbut= new Button();
-            resbut.setLayoutY(resourceSupplyY+width/16);
-            resbut.setLayoutX(width/6+ width/12 + (i)*width/6 + resourcesOffsetX);
-            ImageView tempImage=new ImageView(res.get(i));
+            ImageView tempImage=resourceImageViews.get(i);
             tempImage.setPreserveRatio(true);
+            tempImage.setLayoutY(resourceSupplyY+width/16);
+            tempImage.setLayoutX(width/6+ width/12 + (i)*width/6 + resourcesOffsetX);
             tempImage.setFitWidth(50);
-            resbut.setId("resourceButton");
-            resbut.setGraphic(tempImage);
-            setupAnchor.getChildren().add(resbut);
-            sceneResources.add(resbut);
+            tempImage.setId("resourceButton");
+            setupAnchor.getChildren().add(tempImage);
             selectedResources.add(0);
 
         }
 
         double resourcesX=width/2;
         ViewPersonalBoard.getController().setAllowedRes(new int[]{3,3,3,3});
-        ViewPersonalBoard.getController().bindDispenser(selectedResources,sceneResources,Util.resourcesToChooseOnSetup(getClient().getCommonData().getThisPlayerIndex()));
-        selectedResources.addListener((ListChangeListener<Integer>) c -> {
-            c.next();
-            ImageView tempImage=new ImageView(res.get(c.getFrom()));
-            tempImage.setLayoutX(resourcesX);
-            tempImage.setLayoutY(len/2 - 50);
-            setupAnchor.getChildren().add(tempImage);
-            //getClient().getStage().getScene().setCursor(new ImageCursor(Color.getRes().get(c.getFrom())));
-        });
+        //ViewPersonalBoard.getController().bindDispenser(selectedResources,sceneResources,Util.resourcesToChooseOnSetup(getClient().getCommonData().getThisPlayerIndex()));
+        ViewPersonalBoard.getController().bindDispenserFromImage(selectedResources,resourceImageViews,Util.resourcesToChooseOnSetup(getClient().getCommonData().getThisPlayerIndex()));
+
+        //todo add animation on selected resources
+
         setupAnchor.getChildren().add(validationButton());
         setupAnchor.setId("background");
         getClient().getStage().show();
