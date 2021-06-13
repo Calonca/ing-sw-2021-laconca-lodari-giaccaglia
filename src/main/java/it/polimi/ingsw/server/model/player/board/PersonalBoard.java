1460dd74dea689aef09b175b1f446b6356ee8b96
calonca <alexlaconca@gmail.com>
2021-06-13 18:20:12 +0200

    Enable choosing res in strongbox for production

--- src/main/java/it/polimi/ingsw/server/model/player/board/PersonalBoard.java
+++ src/main/java/it/polimi/ingsw/server/model/player/board/PersonalBoard.java
@@ -86,10 +86,11 @@ public class PersonalBoard {
         strongBox = Box.strongBox();
         discardBox = Box.discardBox();
         discardBox.addResources(new int[]{3,3,3,3});  //setup phase DiscardBox
-        strongBox.addResources(new int[]{20,15,12,30});   //to test CardShop
-        warehouseLeadersDepots.addResource(new Pair<>(0,Resource.GOLD));
-        warehouseLeadersDepots.addResource(new Pair<>(1,Resource.SERVANT));
-        warehouseLeadersDepots.addResource(new Pair<>(3,Resource.STONE));
+        //strongBox.addResources(new int[]{20,15,12,30});   //to test CardShop
+        //warehouseLeadersDepots.addResource(new Pair<>(0,Resource.GOLD));
+        //warehouseLeadersDepots.addResource(new Pair<>(1,Resource.SERVANT));
+        //warehouseLeadersDepots.addResource(new Pair<>(3,Resource.STONE));
+
         productions = Stream.of(Optional.of(Production.basicProduction())).collect(Collectors.toList());
         productions.add(Optional.empty());
         productions.add(Optional.empty());
