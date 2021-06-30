1e1abacbd073a0562743250e1cb0703a110fd2f7
m3f157O <gianmarco.lodari@mail.polimi.it>
2021-06-30 20:04:16 +0200

    Fixing refresh and introducing production condition

--- src/main/java/it/polimi/ingsw/client/view/GUI/MiddlePhaseGUI.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI/MiddlePhaseGUI.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.client.view.GUI;
 
 import it.polimi.ingsw.client.view.abstractview.MiddlePhaseViewBuilder;
 import it.polimi.ingsw.network.simplemodel.SimpleCardShop;
+import it.polimi.ingsw.network.simplemodel.SimpleProductions;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.SubScene;
@@ -30,7 +31,6 @@ public class MiddlePhaseGUI extends MiddlePhaseViewBuilder implements GUIView {
         SubScene root=getRoot();
         root.setId("MIDDLE");
         GUI.addLast(root);
-        Playground.getPlayground().refreshMarket();
 
     }
 
@@ -73,18 +73,25 @@ public class MiddlePhaseGUI extends MiddlePhaseViewBuilder implements GUIView {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        //TODO FIX MARKET REFRESH
         Playground.refreshCardShop();
+        Playground.refreshMarket();
 
         cardButton.setOnAction( p ->
         {
             sendChoice(Choice.CARD_SHOP);
         });
         SimpleCardShop simpleCardShop=getSimpleModel().getElem(SimpleCardShop.class).orElseThrow();
-        System.out.println(simpleCardShop.getIsAnyCardPurchasable());
+
+        //TODO FIX
         if(!simpleCardShop.getIsAnyCardPurchasable())
             cardButton.setDisable(true);
 
         productionButton.setOnAction( e -> sendChoice(Choice.PRODUCTION));
+
+        SimpleProductions simpleProductions=getThisPlayerCache().getElem(SimpleProductions.class).orElseThrow();
+
+        productionButton.setDisable(!simpleProductions.isAnyProductionAvailable());
 
         resourceMarketButton.setOnAction( e -> sendChoice(Choice.RESOURCE_MARKET));
         middleAnchor.setId("middlePane");
